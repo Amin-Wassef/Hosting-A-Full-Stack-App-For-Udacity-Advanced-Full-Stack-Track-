ae5ab74a35fc3b46c278dd1907f3f0d8cfe1916e
Amin Wassef <dr.amin.wassef@gmail.com>
2023-02-12 23:36:43 +0300

    Push Trial With circleci

--- udagram/udagram-api/src/server.ts
+++ udagram/udagram-api/src/server.ts
@@ -49,7 +49,7 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 
   // Root URI call
   app.get("/", async (req, res) => {
-    res.send("/api/v0/");
+    res.send("This is the circleci deployment after a git push");
   });
 
   // Start the Server
